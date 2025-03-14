8667a7ce0a4e21e8acc7226fe069dd1127df7741
Mars0827 <marsbenitez1@gmail.com>
2025-03-14 21:04:52 +0800

    updated server and added api functions

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from "dotenv";
 import { connectDB } from './config/db.js';
 import Product from './models/product.model.js';
+import mongoose from 'mongoose';
+import productRoutes from "./routes/product.route.js";
 
 
 dotenv.config();
@@ -10,26 +12,7 @@ const app = express();
 
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
-app.post("/api/products", async (req, res) => {
-    const product = req.body; // user will send this data
-
-    if(!product.name || !product.price || !product.image){
-        return res.status(400).json({success:false, message: "Please provide all fields."});
-    }
-
-    const newProduct = new Product(product);
-
-    try{
-        await newProduct.save();
-        res.status(201).json({success:true, data: newProduct});
-    } catch (error) {
-        console.error("Error in Create Product: ", error.message);
-        res.status(500).json({ success: false, message: "Server Error" });
-    }
-});
-
-console.log(process.env.MONGO_URI);
-
+app.use("/api/products", productRoutes);
 app.listen(5000, () => {
     connectDB();
     console.log('Server start at http://localhost:5000');
